d2d4b07cb8b3801fd8dcdbb34ba565005783bd01
Mustufa786 <mustufa_ayub@hotmail.com>
2019-08-20 12:05:16 +0500

    login screen added

--- app/src/main/java/edu/aku/hassannaqvi/src_2/ui/LoginActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/src_2/ui/LoginActivity.java
@@ -10,6 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.activity_login);
     }
 }
