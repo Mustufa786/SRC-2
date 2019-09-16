2eaf83e91479b1905b505824b77afd97095b7dd4
syed.irfanullah <irfansyed.pg@gmail.com>
2019-09-16 10:23:35 +0500

    commit

--- app/src/main/java/edu/aku/hassannaqvi/src_2/ui/form7/F7SectionAActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/src_2/ui/form7/F7SectionAActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.aku.hassannaqvi.src_2.R;
 import edu.aku.hassannaqvi.src_2.core.DatabaseHelper;
@@ -35,6 +39,14 @@ public class F7SectionAActivity extends AppCompatActivity {
     }
 
     private void setupViews() {
+
+        List<String> list = new ArrayList<>();
+        list.add("-Select Child-");
+        assert MainApp.lst_U5 != null;
+        list.addAll(MainApp.lst_U5);
+
+        bi.f7a01.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list));
+
 
         bi.f7a04.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -82,18 +94,19 @@ public class F7SectionAActivity extends AppCompatActivity {
 
         JSONObject f1 = new JSONObject();
 //        f1.put("f7a01", bi.f7a01.getText().toString());
+        f1.put("f7a01", bi.f7a01.getSelectedItem().toString());
         f1.put("f7a02a", bi.f7a02a.getText().toString());
         f1.put("f7a02b", bi.f7a02b.getText().toString());
         f1.put("f7a02c", bi.f7a02c.getText().toString());
-        f1.put("f7a03",bi.f7a03a.isChecked() ?"1" :bi.f7a03b.isChecked() ?"2" :"0");
-        f1.put("f7a04",bi.f7a04a.isChecked() ?"1" :bi.f7a04b.isChecked() ?"2" :bi.f7a0498.isChecked() ?"98" :"0");
-        f1.put("f7a05",bi.f7a05a.isChecked() ?"1" :bi.f7a05b.isChecked() ?"2" :bi.f7a0598.isChecked() ?"98" :"0");
-        f1.put("f7a06",bi.f7a06a.isChecked() ?"1" :bi.f7a06b.isChecked() ?"2" :bi.f7a0698.isChecked() ?"98" :"0");
+        f1.put("f7a03", bi.f7a03a.isChecked() ? "1" : bi.f7a03b.isChecked() ? "2" : "0");
+        f1.put("f7a04", bi.f7a04a.isChecked() ? "1" : bi.f7a04b.isChecked() ? "2" : bi.f7a0498.isChecked() ? "98" : "0");
+        f1.put("f7a05", bi.f7a05a.isChecked() ? "1" : bi.f7a05b.isChecked() ? "2" : bi.f7a0598.isChecked() ? "98" : "0");
+        f1.put("f7a06", bi.f7a06a.isChecked() ? "1" : bi.f7a06b.isChecked() ? "2" : bi.f7a0698.isChecked() ? "98" : "0");
         f1.put("f7a0796x", bi.f7a0796x.getText().toString());
-        f1.put("f7a07",bi.f7a07a.isChecked() ?"1" :bi.f7a07b.isChecked() ?"2" :bi.f7a07c.isChecked() ?"3" :bi.f7a07d.isChecked() ?"4" :bi.f7a07e.isChecked() ?"5" :bi.f7a07f.isChecked() ?"6" :bi.f7a07g.isChecked() ?"7" :bi.f7a07h.isChecked() ?"8" :bi.f7a07i.isChecked() ?"9" :bi.f7a07j.isChecked() ?"10"  :bi.f7a07k.isChecked() ?"11" :bi.f7a07l.isChecked() ?"12" :bi.f7a07m.isChecked() ?"13" :bi.f7a07n.isChecked() ?"14" :bi.f7a0796.isChecked() ?"96" :bi.f7a0798.isChecked() ?"98" :"0");
+        f1.put("f7a07", bi.f7a07a.isChecked() ? "1" : bi.f7a07b.isChecked() ? "2" : bi.f7a07c.isChecked() ? "3" : bi.f7a07d.isChecked() ? "4" : bi.f7a07e.isChecked() ? "5" : bi.f7a07f.isChecked() ? "6" : bi.f7a07g.isChecked() ? "7" : bi.f7a07h.isChecked() ? "8" : bi.f7a07i.isChecked() ? "9" : bi.f7a07j.isChecked() ? "10" : bi.f7a07k.isChecked() ? "11" : bi.f7a07l.isChecked() ? "12" : bi.f7a07m.isChecked() ? "13" : bi.f7a07n.isChecked() ? "14" : bi.f7a0796.isChecked() ? "96" : bi.f7a0798.isChecked() ? "98" : "0");
 
         f1.put("f7a0896x", bi.f7a0896x.getText().toString());
-        f1.put("f7a08",bi.f7a08a.isChecked() ?"1" :bi.f7a08b.isChecked() ?"2" :bi.f7a0896.isChecked() ?"96" :"0");
+        f1.put("f7a08", bi.f7a08a.isChecked() ? "1" : bi.f7a08b.isChecked() ? "2" : bi.f7a0896.isChecked() ? "96" : "0");
 
         MainApp.fc.setF7(String.valueOf(f1));
 
